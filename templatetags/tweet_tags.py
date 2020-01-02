f89d908bfcd943232ef511b4e9d7aed5b71a6d02
Chris <8351123+z3ke1r@users.noreply.github.com>
2020-01-02 08:50:56 -0700

    Updated code style

--- templatetags/tweet_tags.py
+++ templatetags/tweet_tags.py
@@ -4,8 +4,11 @@ from django import template
 register = template.Library()
 
 @register.simple_tag
-def tweet_tags(url):
-    twtjson = requests.get('https://publish.twitter.com/oembed?url=' + url + '&omit_script=true')
-    twtparse = twtjson.json()
-    twthtml = twtparse['html']
-    return twthtml
+def tweet_tags(url:string) -> string:
+    """ Requests a tweet from oembed and returns the html element """
+    
+    tweet_request = requests.get('https://publish.twitter.com/oembed?url=' + url + '&omit_script=true')
+    tweet_json = tweet_request.json()
+    tweet_html = tweet_json['html']
+    
+    return tweet_html
